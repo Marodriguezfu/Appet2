ff3d8cbc77246d71639af3dbcc3ef21cf66eeee8
Carlos Jiménez <68668422+cajimn@users.noreply.github.com>
2020-08-13 12:02:44 -0500

    Cree la pagina de registro y las actividades de sus botones conectandolas con las otras actividades

--- app/src/main/java/com/example/appet/LoginActivity.kt
+++ app/src/main/java/com/example/appet/LoginActivity.kt
@@ -5,46 +5,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_login.password_login
-import kotlinx.android.synthetic.main.activity_login.user_login
 
-class AuthActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        //Hacemos que una vez, la aplicación ha cargado, empiece a ejecutarse la app
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_login)
 
-        //setup
-        setup()
-
-        ir_a_login_button.setOnClickListener {
-            val loginIntent = Intent(this, LoginActivity::class.java)
-            startActivity(loginIntent)
-
+        sign_in_button.setOnClickListener {
+            val registerIntent = Intent(this, AuthActivity::class.java)
+            startActivity(registerIntent)
         }
-    }
 
+        //Setup
+        setup()
+    }
     //FUNCION SETUP
     private fun setup() {
-        title = "Autenticación"
+        title = "Inicio de sesión"
 
-        //CREAR UN USUARIO
-        sign_up_button.setOnClickListener {//RECOGER EL EVENTO CUANDO SE EJECUTE
-            if (user_login.text.isNotEmpty() && password_login.text.isNotEmpty() && nombre_registro.text.isNotEmpty() && telefono_registro.text.isNotEmpty()) { //COMPROBAR QUE NO SON VACÍAS
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(user_login.text.toString(), password_login.text.toString()).addOnCompleteListener {//REGISTRO DE USUARIO Y CONTRASEÑA EN FIREBASE
+        //ENTRAR CON UN USUARIO REGISTRADO
+        log_in_button.setOnClickListener {
+            if (user_login.text.isNotEmpty() && password_login.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(user_login.text.toString(),password_login.text.toString()).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC) // PASAR A LA NUEVA PANTALLA,los signos de interrogación son porque el email puede o no existir( Por lo que estas son condiciones por si no existe envíe un string vacío
-                    } else {
-                        showAlert()//El usuario ya esta registrado
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC) //los signos de interrogación son porque el email puede o no existir( Por lo que estas son condiciones por si no existe envíe un string vacío
+                    }else {
+                        showAlert() //SI NO SE REGISTRA CREA UNA ALERTA
                     }
                 }
             }else {
                 showAlert() //Si estan vacios los campos
             }
+            }
         }
-    }
+
+
+
+    //FUNCION QUE PRODUCE UNA ALERTA SI ALGO ESTA MAL
     private fun showAlert(){
 
         val builder = AlertDialog.Builder(this)
@@ -65,7 +67,6 @@ class AuthActivity : AppCompatActivity() {
         startActivity(homeIntent) //LA NAVEGACION A LA NUEVA PANTALLA
         finish()
     }
-
 
 
 }
