33011e2892b2e2d0e84dd71f90e897a83dd87cdf
GabrielBojaca <gabriel.bojacam@hotmail.com>
2020-12-06 02:53:19 -0500

    Implementacion Calendario vacunas beta

--- app/src/main/java/com/example/appet/RegistroVacunasActivity.kt
+++ app/src/main/java/com/example/appet/RegistroVacunasActivity.kt
@@ -1,7 +1,13 @@
 package com.example.appet
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_registro_vacunas.*
+import java.util.*
 
 class RegistroVacunasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +19,15 @@ class RegistroVacunasActivity : AppCompatActivity() {
         val nombre =  bundle?.getString("nombreMascota")
         println("El patas es: $nombre")
 
+        etDate.setOnClickListener{showDatePickerDialog()}
 
+    }
 
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+    private fun onDateSelected(day: Int, month: Int, year: Int) {
+        etDate.setText(day.toString()+"/"+month.toString()+"/"+year.toString())
     }
 }
