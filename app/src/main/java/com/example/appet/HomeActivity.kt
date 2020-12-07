ec18b6f6c1b5596bfffc4ff9dec79608f1310864
allangsg <aserrato@unal.edu.co>
2020-12-07 03:47:27 -0500

    listeners del menu de barra y previo a implementación fragments

--- app/src/main/java/com/example/appet/HomeActivity.kt
+++ app/src/main/java/com/example/appet/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.home_content.*
@@ -58,6 +60,44 @@ class HomeActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        //super.onBackPressed()
+        super.onBackPressed()
     }
+
+    /**
+     * Liseteners para cada ietm en el menu de la barra de la app
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.mascotas_opt ->{
+                //Agregar accion que desee que haga este botón
+                true
+            }
+            R.id.galleria_opt ->{
+                //Agregar acción que desee que haga este botón
+                true
+            }
+            R.id.settings_btn ->{
+                //Agregar Acción que desee que haga este botón.
+              true
+            }
+            R.id.log_out_opt ->{
+                //Acciones para realizar un LogOut exitoso
+                //Borrado de datos
+                val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE). edit()
+                prefs.clear()
+                prefs.apply()
+
+                FirebaseAuth.getInstance().signOut()
+                //onBackPressed()//para volver a la pantalla anterior(EL INICIO DE SESION)
+                finish()
+                val loginIntent = Intent(this, LoginActivity::class.java)
+                startActivity(loginIntent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+
 }
